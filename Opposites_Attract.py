0bf512a9844c2290fc5bcd6832b1bc7840cfca7d
Rony Richard <rony.richard11@gmail.com>
2023-04-10 17:53:11 -0400

    April 10th, 2023

---
+++ Opposites_Attract.py
@@ -0,0 +1,10 @@
+# 10th April, 2023
+# codewars.com/kata/555086d53eac039a2a000083/train/python
+
+def lovefunc(flower1, flower2):
+    if flower1 % 2 == 0 and flower2 % 2 != 1 or flower1 % 2 == 1 and flower2 % 2 != 0:
+        print("InCompatible")
+        return False
+    else:
+        print("Compatible")
+        return True
