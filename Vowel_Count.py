e1cccccbacba067ee958e3cc50f4a4469043971c
Rony Richard <rony.richard11@gmail.com>
2023-04-10 16:39:18 -0400

    April 10th, 2023

---
+++ Vowel_Count.py
@@ -0,0 +1,14 @@
+# 10th April 2023
+# Link: codewars.com/kata/54ff3102c1bad923760001f3/train/python
+
+def get_count(sentence):
+    vowel_count = 0
+    vowels = "aeiou"
+    for i in sentence:
+        if i in vowels:
+            vowel_count += 1
+    print(vowel_count)
+    return vowel_count
+
+
+
